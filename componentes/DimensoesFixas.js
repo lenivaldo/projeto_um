11bab65e8a399a17f537e6a4ad37019e1e34f960
Lenivaldo <lenivaldoreis@outlook.com>
2022-05-15 18:41:57 -0300

    justifyContent

--- componentes/DimensoesFixas.js
+++ componentes/DimensoesFixas.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { View } from "react-native";
 
-//Estudando layout
+/*Estudando layout
+//flexDirection: 'column', 'row'
+//justifyContent: 'flex-start', 'center', 'flex-end', 'space-between','space-around'
+//
+//
+*/
 
 class DimensoesFixas extends React.Component{
     render(){
         return(
-            <View style={{width: '100%', height: '100%', flexDirection: 'column'}}>
+            <View style={{width: '100%', height: '100%', flexDirection: 'column', justifyContent: 'space-around'}}>
                 <View style={{width: 150, height: 150, backgroundColor: 'powderblue'}}/>
                 <View style={{width: 150, height: 150, backgroundColor: 'skyblue'}}/>
                 <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}}/>
