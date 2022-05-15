cd121905bb491d5662f5029e1a3ceb8c5a465b68
Lenivaldo <lenivaldoreis@outlook.com>
2022-05-15 19:04:17 -0300

    indice flexWrap

--- componentes/DimensoesFixas.js
+++ componentes/DimensoesFixas.js
@@ -4,17 +4,19 @@ import { View } from "react-native";
 /*Estudando layout
 //flexDirection: 'column', 'row'
 //justifyContent: 'flex-start', 'center', 'flex-end', 'space-between','space-around'
-//
-//
+//alignItens: 'stretch', 'flex-start', 'flex-end', 'center', 'baseline'
+//flexWrap: 'nowrap', 'wrap', 'wrap-reverse'
+//flexgrow: 
 */
 
 class DimensoesFixas extends React.Component{
     render(){
         return(
-            <View style={{width: '100%', height: '100%', flexDirection: 'column', justifyContent: 'space-around'}}>
-                <View style={{width: 150, height: 150, backgroundColor: 'powderblue'}}/>
-                <View style={{width: 150, height: 150, backgroundColor: 'skyblue'}}/>
-                <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}}/>
+            <View style={{width: '100%', height: '100%', flexDirection: 'column',
+            justifyContent: 'center', alignItems: 'stretch'}}>
+                <View style={{height: 50, backgroundColor: 'powderblue'}}/>
+                <View style={{height: 50, backgroundColor: 'skyblue'}}/>
+                <View style={{height: 50, backgroundColor: 'steelblue'}}/>
             </View>
         );
     }
